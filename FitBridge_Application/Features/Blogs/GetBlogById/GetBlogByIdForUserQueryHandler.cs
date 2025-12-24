6df235c2156d2b5f6f08bfe4fbcf74dd06a84226
imbatcat <ptdat04@gmail.com>
2025-12-24 16:18:37 +0700

    Update GetBlogByIdForUserQueryHandler.cs

--- FitBridge_Application/Features/Blogs/GetBlogById/GetBlogByIdForUserQueryHandler.cs
+++ FitBridge_Application/Features/Blogs/GetBlogById/GetBlogByIdForUserQueryHandler.cs
@@ -20,7 +20,6 @@ internal class GetBlogByIdForUserQueryHandler(IUnitOfWork unitOfWork) : IRequest
             Id = blog.Id,
             Title = blog.Title,
             Content = blog.Content,
-            AuthorId = blog.AuthorId,
             Images = blog.Images,
             CreatedAt = blog.CreatedAt,
             UpdatedAt = blog.UpdatedAt,
